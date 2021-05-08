bc3fa79bda87653d46e7bf7988aea49122d72edd
M1sterMass <alexmass1990@gmail.com>
2021-05-08 22:19:49 +0300

    first step

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+
+
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:x8Tmpq093@127.0.0.1:5432/todoapp'
+
